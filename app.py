44b0d0ff3ec3d570405291a13b537097b964f2aa
NIKITAKHAKRE <nikitakhakre747@gmail.com>
2025-09-01 23:17:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+import joblib
+
+app = Flask(__name__)
+model = joblib.load('spam_model.pkl')
+vectorizer = joblib.load('vectorizer.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    message = data.get('message', '')
+    vec = vectorizer.transform([message])
+    prediction = model.predict(vec)[0]
+    return jsonify({'prediction': 'spam' if prediction else 'ham'})
+
+if __name__ == '__main__':
+    app.run(debug=True)
